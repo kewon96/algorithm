96be848b3baa02ed67af9a8aec4015dbd14e53bd
Kewon <yang0184@naver.com>
2019-07-29 18:01:02 +0900

    선형검색

--- SeqSerch.java
+++ SeqSerch.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
 
+//선형검색
 class SeqSerch {
     //a : 배열의 원소수, n : 배열의 길이(=한계값), key : 찾는 값
+    //찾는 값을 배열 맨 끝에 저장한 뒤 하나하나 진행할 때 마다 맨 끝값을 비교하는 방식
     static int seqSerch(int[] a, int n, int key) {
-        for(int i=0; i<n; i++) {
-            if(a[i] == key) return i;
+        // for(int i=0; i<n; i++) {
+        //     if(a[i] == key) return i;
+        // }
+        // return -1;
+        int i=0;
+
+        a[n]=key; //찾을 값이 맨 끝에 저장된 상황
+
+        while(true) {
+            if(a[i]==key) break;
+            
+            i++;
         }
-        return -1;
+
+        return i==n? -1 : i;
     }
 
     public static void main(String[] args) {
@@ -16,7 +29,7 @@ class SeqSerch {
         //num : 배열 수
         int num = sc.nextInt();
         //x배열 선언
-        int[] x=new int[num];
+        int[] x=new int[num+1];
 
         //x배열에 입력값을 넣음
         for(int i=0; i<num; i++) {
