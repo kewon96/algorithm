8310d576c560f036ad2624e169eadb72c0f5c24a
Kewon <yang0184@naver.com>
2019-07-31 14:28:11 +0900

    범위내의 제곱수 출력, 1324패턴 출력

---
+++ Quest.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Quest {
+    Scanner sc = new Scanner(System.in);
+
+    public void Quest1() {
+        System.out.print("최대값 : ");
+        int a=sc.nextInt(); //제한값 삽입
+        int exp=1;
+        int i=1;
+        int j=1;
+
+        for (; i<10000; i++) {
+            for (; j<10000; j++) {
+                if(i==j) {
+                    exp=i*j;
+                    if(exp<a) {
+                        System.out.println(exp);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // 1,2,3,4,5,6,7,8
+    // 1,3,2,4,5,7,6,8, ...
+    // +2,-1,+2,-1...
+    public void Quest2() {
+        System.out.print("최대값 : ");
+        int a=sc.nextInt();
+
+            int count=0;
+
+        for(int i=1; count<a; i=i+2){
+            count=2*i-1;
+            if(count<a) System.out.println(count);
+            else continue;
+
+            count=2*i+1;
+            if(count<a) System.out.println(count);
+            else continue;
+
+            count=2*i;
+            if(count<a) System.out.println(count);
+            else continue;
+
+            count=2*i+2;
+            if(count<a) System.out.println(count);
+            else continue;
+
+            //i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Quest q = new Quest();
+
+        //q.Quest1();
+        q.Quest2();
+    }
+}
