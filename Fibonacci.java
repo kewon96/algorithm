23712fa68664035c9467318f7475e0f53c17aaf9
Kewon <yang0184@naver.com>
2019-07-31 14:27:19 +0900

    피보나치수열

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 //피보나치수열
 class Fibonacci {
     static int fibonacci(int a) {
-            if(a==1) return 0;
-            else if(a==2) return 1;
-            else return fibonacci(a-1)+fibonacci(a-2);
+        if(a==1) return 0;
+        else if(a==2) return 1;
+        else return fibonacci(a-1)+fibonacci(a-2);
     }
     public static void main(String args[]) {
 
