e530fed2d520be4afbffee7f4e0064d6c2f1b36d
Kewon <yang0184@naver.com>
2019-08-05 19:23:34 +0900

    세자리 숫자의 백의 자리수와 일의 자리수를 바꾼 뒤 비교하기 -예외처리

--- Compare_and_replace.java
+++ Compare_and_replace.java
@@ -2,16 +2,39 @@ import java.util.Scanner;
 
 //세자리 숫자의 백의 자리수와 일의 자리수를 바꾼 뒤 비교하기
 class Compare_and_replace {
-    public void compare(String str) {
+    //
+    public boolean filter(int a, int b) {
+        /**
+         * 두 수가 같거나
+         * 범위외의 값이거나
+         * 0이 포함되있다면 false
+         */
+        String alpha=Integer.toString(a);
+        String beta=Integer.toString(b);
+        
+        if((a==b)||(a<100&&a>999)||(b<100&&b>999)||alpha.contains("0")||beta.contains("0")) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //앞 뒤 바꾸기
+    public int compare(int i) {
+        String str=Integer.toString(i);
         
         String str_copy="";
         char[] chr = str.toCharArray();
 
-        for(int i=2; i>=0; i--) {
-            str_copy=str_copy+Character.toString(chr[i]);
+        for(int j=2; j>=0; j--) {
+            str_copy=str_copy+Character.toString(chr[j]);
         }
 
         str=str_copy;
+
+        i=Integer.parseInt(str_copy);
+
+        return i;
     }
 
     public static void main(String[] a) {
@@ -20,17 +43,23 @@ class Compare_and_replace {
 
         System.out.println("숫자 입력부터!");
         System.out.print("첫번째 : ");
-        String first=sc.next();
+        int first=sc.nextInt();
         System.out.print("두번째 : ");
-        String second=sc.next();
+        int second=sc.nextInt();
 
-        cr.compare(first);
-        cr.compare(second);
+        boolean check = cr.filter(first, second);
 
-        int f=Integer.parseInt(first);
-        int s=Integer.parseInt(second);
+        if(check==true) {
+            int f=cr.compare(first);
+            int s=cr.compare(second);
+    
+            if(f>s) System.out.println(f);
+            else System.out.println(s);
+        }
+        else {
+            System.out.println("제대로 입력안했구나");
+        }
 
-        if(f>s) System.out.println(f);
-        else System.out.println(s);
+        sc.close();
     }
 }
